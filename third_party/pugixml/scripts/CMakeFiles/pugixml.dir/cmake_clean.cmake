b0b38a579a1f7c82cb40e77b74cd0cc141306ed9
shawn <linpackzh@gmail.com>
2014-01-08 00:21:14 +0800

    add rex-matching-engine

---
+++ third_party/pugixml/scripts/CMakeFiles/pugixml.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/pugixml.dir/mnt/ssd/coinport/coinport/src/third_party/rex-matching-engine-read-only/src/pugixml/src/pugixml.cpp.o"
+  "libpugixml.pdb"
+  "libpugixml.a"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/pugixml.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
